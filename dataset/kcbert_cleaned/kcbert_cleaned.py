a2b49921cfa2eae0cb3e924d6dc71bb7125f5eae
Quasar Kim <vhfflrhs128@gmail.com>
2022-12-26 20:11:08 +0900

    Change dataset backend to dask

--- dataset/kcbert_cleaned/kcbert_cleaned.py
+++ dataset/kcbert_cleaned/kcbert_cleaned.py
@@ -1,32 +1,27 @@
-import tensorflow as tf
-import tensorflow_datasets as tfds
-import tensorflow_io as tfio
+from dask.distributed import Client
+import dask.dataframe as dd
+from tensorflow_datasets import features
+from tensorflow_datasets.core import GeneratorBasedBuilder, Version
+from tensorflow_datasets.core.dataset_info import DatasetInfo
 
-class KcbertCleaned(tfds.dataset_builders.TfDataBuilder):
-    def __init__(self, **kwargs):
-        super().__init__(
-            name='kcbert_cleaned',
-            version='1.0.0',
-            split_datasets=self._get_splits(),
-            features=tfds.features.FeaturesDict({
-                'text': tfds.features.Text()
-            }),
-            **kwargs
+class KcbertCleaned(GeneratorBasedBuilder):
+    VERSION = Version('1.0.0')
+
+    def _info(self):
+        return DatasetInfo(
+            builder=self,
+            features=features.FeaturesDict({
+                'text': features.Text()
+            })
         )
 
-    def _get_splits(self):
-        ds_option = tf.data.Options()
-        ds_option.experimental_deterministic = False
-
-        ds = tf.data.Dataset.list_files('gs://kc-moe/dataset/parquet/kcbert-cleaned/*.parquet', shuffle=False)
-        ds = ds.with_options(ds_option)
-        ds = ds.interleave(
-            lambda file: tfio.IODataset.from_parquet(file, columns={
-                'text': tf.TensorSpec(shape=(), dtype=tf.string)
-            }),
-            cycle_length=5,
-            num_parallel_calls=tf.data.AUTOTUNE
-        )
+    def _split_generators(self, *args):
         return {
-            'train': ds
+            'train': self._generate_examples()
         }
+
+    def _generate_examples(self):
+        client = Client() # initialize distributed cluster
+        df = dd.read_parquet('gs://kc-moe/dataset/parquet/kcbert-cleaned', engine='fastparquet')
+        for index, row in df.iterrows():
+            yield index, row
--- dataset/tfds_test/tfds_test.py
+++ dataset/tfds_test/tfds_test.py
@@ -1,18 +1,43 @@
 """tfds_test dataset."""
 
 import tensorflow_datasets as tfds
+from tensorflow_datasets.core.dataset_info import DatasetInfo
+from tensorflow_datasets.core import GeneratorBasedBuilder
 import tensorflow_io as tfio
+import pandas as pd
 
-class TfdsTest(tfds.dataset_builders.TfDataBuilder):
-  def __init__(self, **kwargs):
-    super().__init__(
-      name='tfds_test',
-      version='1.0.0',
-      split_datasets={
-        'train': tfio.IODataset.from_parquet('gs://kc-moe/dataset/parquet/kcbert-cleaned/part.0.parquet', columns=['text'])
-      },
+# class TfdsTest(tfds.dataset_builders.TfDataBuilder):
+#   def __init__(self, **kwargs):
+#     super().__init__(
+#       name='tfds_test',
+#       version='1.0.0',
+#       split_datasets={
+#         'train': tfio.IODataset.from_parquet('deleteme2.parquet', columns=['text'])
+#       },
+#       features=tfds.features.FeaturesDict({
+#         'text': tfds.features.Text()
+#       }),
+#       **kwargs
+#     )
+
+class TfdsTest(GeneratorBasedBuilder):
+  VERSION = tfds.core.Version('1.0.0')
+
+  def _info(self):
+    return DatasetInfo(
+      builder=self,
       features=tfds.features.FeaturesDict({
         'text': tfds.features.Text()
-      }),
-      **kwargs
+      })
     )
+  
+  def _split_generators(self, *args):
+    return {
+      'train': self._generate_examples()
+    }
+
+  def _generate_examples(self):
+    df = pd.read_parquet('gs://kc-moe/dataset/parquet/kcbert-cleaned/part.0.parquet')
+    for i, row in df.iterrows():
+      example = row.to_dict()
+      yield i, example

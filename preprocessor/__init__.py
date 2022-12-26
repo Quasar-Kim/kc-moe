fdfe2b1b948afcc9ff6599393250da9775ef2973
Quasar Kim <vhfflrhs128@gmail.com>
2022-12-26 20:10:09 +0900

    Implement retokenize preprocessor

---
+++ preprocessor/__init__.py
@@ -0,0 +1,1 @@
+from .retokenize import retokenize
--- task/tfds_test.py
+++ task/tfds_test.py
@@ -1,24 +1,19 @@
-import tfds_test
 import seqio
+from preprocessor import retokenize
+from functools import partial
+import tensorflow as tf
 
 seqio.TaskRegistry.add(
     name='tfds_test',
-    source=seqio.TfdsDataSource('tfds_test:1.0.0', splits={ 'train': 'train[:75%]', 'test': 'train[75%:]' }),
-    preprocessors=[],
+    source=seqio.TfdsDataSource('tfds_test:1.0.0', splits={ 'train': 'train[:90%]', 'test': 'train[90%:]' }),
+    preprocessors=[
+        partial(retokenize, target_columns=['text'])
+    ],
     output_features={
         'text': seqio.Feature(
-            vocabulary=seqio.PassThroughVocabulary(size=1)
+            vocabulary=seqio.PassThroughVocabulary(size=10),
+            dtype=tf.string,
+            rank=None
         )
     }
 )
-
-ds = seqio.get_mixture_or_task('tfds_test').get_dataset(
-    sequence_length={ 'text': 256 },
-    split='test',
-    shuffle=True,
-    num_epochs=1,
-    use_cached=False,
-    seed=42
-)
-
-next(iter(ds))

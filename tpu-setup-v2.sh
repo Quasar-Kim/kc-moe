b923c5ffa771de7be1eb9c8815c5bcd21bdebed2
Quasar Kim <vhfflrhs128@gmail.com>
2023-01-03 20:23:14 +0000

    Update train setup script

--- tpu-setup-v2.sh
+++ tpu-setup-v2.sh
@@ -4,7 +4,7 @@
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install python3.10 python3.10-distutils -y
+sudo apt install python3.10 python3.10-distutils python3.10-venv -y
 
 # install pip
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
@@ -16,10 +16,14 @@ export PATH="~/.local/bin:$PATH"
 # clone & install all requirements
 git clone https://github.com/quasar-kim/kc-moe
 cd kc-moe
-poetry install --no-root --all-extra
+rm poetry.lock
+poetry install --no-root --all-extras
 
 # clone flaxformer
 git clone https://github.com/google/flaxformer
 mv flaxformer flaxformer-tmp
 mv flaxformer-tmp/flaxformer .
 rm -rf flaxformer-tmp
+
+export GOOGLE_CLOUD_BUCKET_NAME=kc-moe
+export TFDS_DATA_DIR=gs://$GOOGLE_CLOUD_BUCKET_NAME/dataset/tfds
